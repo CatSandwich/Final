3e09ae6076483df7beda3a797ff21b19332a6065
CatSandwich <jtorrington2@gmail.com>
2021-12-03 17:29:34 -0500

    Pushing vector3

---
+++ FinalServer/FinalCommon/Data/Vector3.cs
@@ -0,0 +1,25 @@
+﻿using DarkRift;
+
+namespace FinalCommon.Data
+{
+    public class Vector3 : IDarkRiftSerializable
+    {
+        float X;
+        float Y;
+        float Z;
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            X = e.Reader.ReadSingle();
+            Y = e.Reader.ReadSingle();  
+            Z = e.Reader.ReadSingle();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(X);
+            e.Writer.Write(Y); 
+            e.Writer.Write(Z);
+        }
+    }
+}
