b0110c9407026f7b82ef1e9b3f181e3ae290d5a1
CatSandwich <jtorrington2@gmail.com>
2021-12-03 17:46:39 -0500

    Box

---
+++ FinalServer/FinalCommon/Data/Vector2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DarkRift;
+
+namespace FinalCommon.Data
+{
+    public class Vector2 : IDarkRiftSerializable
+    {
+        public float X;
+        public float Y;
+
+        public void Deserialize(DeserializeEvent e)
+        {
+            X = e.Reader.ReadSingle();
+            Y = e.Reader.ReadSingle();
+        }
+
+        public void Serialize(SerializeEvent e)
+        {
+            e.Writer.Write(X);
+            e.Writer.Write(Y);
+        }
+    }
+}
